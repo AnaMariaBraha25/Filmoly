1fd838be49b96ad05ccf1c9ac596c05d6032f91f
Ana Maria Braha <anamariabraha@gmail.com>
2026-03-19 10:15:06 +0100

    Pantalla principal con 3 vídeos y VideoScreen funcional con VLC

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'video_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,6 +19,27 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  // Vídeos con nombre y URL
+  final List<Map<String, String>> videos = [
+    {
+      'title': 'Dreddo (MP4)',
+      'url':
+          'https://media.retroteca.org/Retroteca/PELICULAS/Dreddo/%281995%29%20Caza%20Legal.%20Andrew%20Sipes.%20ESTADOS%20UNIDOS%20%23122360%20%5B11859%5D%20tt0113010.mp4',
+    },
+    {
+      'title': 'Desperado (MKV)',
+      'url':
+          'https://media.retroteca.org/Retroteca/PELICULAS/Dreddo/%281995%29%20Desperado.%20Robert%20Rodriguez.%20ESTADOS%20UNIDOS%2C%20MEXICO%20%2390911%20%5B1053600%5D%20tt0112851.mkv',
+    },
+    {
+      'title': 'Dororo (AVI)',
+      'url':
+          'https://media.retroteca.org/Retroteca/PELICULAS/dororo/%282005%29%20Leyenda%20Mortal.%20Mark%20Duffield.%20TAILANDIA%20%23130906%20%5B51077%5D%20tt0436359.avi',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,25 +52,32 @@ class HomeScreen extends StatelessWidget {
           children: [
 
             Icon(Icons.movie, size: 80),
-
             SizedBox(height: 20),
-
             Text(
               "Bienvenido",
               style: TextStyle(fontSize: 24),
             ),
-
             SizedBox(height: 40),
 
-           ElevatedButton(
-  onPressed: () {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => VideoScreen()),
-    );
-  },
-  child: Text("Abrir reproductor"),
-),
+            // Botones dinámicos para cada vídeo
+            for (var video in videos)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              VideoScreen(
+                                videoUrl: video['url']!,
+                                title: video['title']!,
+                              )),
+                    );
+                  },
+                  child: Text(video['title']!),
+                ),
+              ),
           ],
         ),
       ),
--- lib/video_screen.dart
+++ lib/video_screen.dart
@@ -1,17 +1,63 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 
-class VideoScreen extends StatelessWidget {
+class VideoScreen extends StatefulWidget {
+  const VideoScreen({super.key, required this.videoUrl, required this.title});
+
+  final String videoUrl;
+  final String title;
+
+  @override
+  State<VideoScreen> createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  VlcPlayerController? _controller;
+  bool _isVlcSupported = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Comprobamos si VLC se puede usar en esta plataforma
+    _isVlcSupported = !kIsWeb &&
+        (defaultTargetPlatform == TargetPlatform.android ||
+            defaultTargetPlatform == TargetPlatform.iOS ||
+            defaultTargetPlatform == TargetPlatform.macOS);
+
+    if (_isVlcSupported) {
+      _controller = VlcPlayerController.network(
+        widget.videoUrl,
+        autoPlay: true,
+        options: VlcPlayerOptions(),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Reproductor de Video"),
+        title: Text(widget.title),
       ),
       body: Center(
-        child: Text(
-          "Reproductor",
-          style: TextStyle(fontSize: 24),
-        ),
+        child: _isVlcSupported && _controller != null
+            ? VlcPlayer(
+                controller: _controller!,
+                aspectRatio: 16 / 9,
+                placeholder: const Center(child: CircularProgressIndicator()),
+              )
+            : const Text(
+                "VLC no soportado en esta plataforma",
+                style: TextStyle(fontSize: 18),
+              ),
       ),
     );
   }
